972fae01f3f18a1968654c1d308f3a0fc66ac0d3
erfan ashki <erfanbayern111@gmail.com>
2024-12-25 14:35:24 +0330

    a few examples & exclude

--- shop/views.py
+++ shop/views.py
@@ -6,6 +6,10 @@ from .models import Product, Discount, Category, Comment, Customer, Address, Car
 
 
 def some_view(request):
-    queryset = Product.objects.filter(name__icontains='work', datetime_created__year=2021, inventory__gt=5)
+    queryset = OrderItem.objects.filter(product__id=4021)
+    queryset = Product.objects.filter(inventory=50)
+    queryset = Product.objects.filter(name__icontains='energy', inventory__gte=66)
+    queryset = Order.objects.filter(status=Order.ORDER_STATUS_UNPAID)
+    queryset = Order.objects.exclude(status=Order.ORDER_STATUS_UNPAID)
     
     return render(request, 'shop/shop.html', {'products': list(queryset)})
