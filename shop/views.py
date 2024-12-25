8f55d4b90a23db576d3651a1d02a2cb7673f6f33
erfan ashki <erfanbayern111@gmail.com>
2024-12-25 15:11:17 +0330

    part two of examples

--- shop/views.py
+++ shop/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from decimal import Decimal
 
 from .models import Product, Discount, Category, Comment, Customer, Address, Cart, CartItem, Order, OrderItem
 
 
 def some_view(request):
-    queryset = OrderItem.objects.filter(product__id=4021)
-    queryset = Product.objects.filter(inventory=50)
-    queryset = Product.objects.filter(name__icontains='energy', inventory__gte=66)
-    queryset = Order.objects.filter(status=Order.ORDER_STATUS_UNPAID)
-    queryset = Order.objects.exclude(status=Order.ORDER_STATUS_UNPAID)
+    queryset = OrderItem.objects.filter(order_id=70).exclude(quantity__gt=10)
+    queryset = Product.objects.filter(unit_price__lt=Decimal('780.5'))
+    queryset = Order.objects.filter(customer__first_name__icontains='Kimberly')
     
     return render(request, 'shop/shop.html', {'products': list(queryset)})
