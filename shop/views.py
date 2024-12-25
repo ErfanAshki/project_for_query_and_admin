281a9ff93b53e6f22c240edbf96fb0c9455c0056
erfan ashki <erfanbayern111@gmail.com>
2024-12-25 15:58:51 +0330

    Q objects and ~ & startswith -istartswith lookups

--- shop/views.py
+++ shop/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from decimal import Decimal
+from django.db.models import Q, F
 
 from .models import Product, Discount, Category, Comment, Customer, Address, Cart, CartItem, Order, OrderItem
 
 
 def some_view(request):
-    queryset = OrderItem.objects.filter(order_id=70).exclude(quantity__gt=10)
-    queryset = Product.objects.filter(unit_price__lt=Decimal('780.5'))
-    queryset = Order.objects.filter(customer__first_name__icontains='Kimberly')
+    queryset = Product.objects.filter(Q(inventory__lt=3) | Q(inventory__gt=95))
+    queryset = Product.objects.filter(~Q(inventory__lt=50) | Q(name__startswith='s'))
+    queryset = Product.objects.filter(~Q(inventory__lt=50) | Q(name__istartswith='s'))
+    queryset = OrderItem.objects.filter(~Q(quantity__gt=8))
     
     return render(request, 'shop/shop.html', {'products': list(queryset)})
