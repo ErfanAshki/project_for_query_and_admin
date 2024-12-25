bc4218720e69183cf1b048c1ac8bf46b2e5098f9
erfan ashki <erfanbayern111@gmail.com>
2024-12-25 13:54:22 +0330

    year - month - day lookup

--- shop/views.py
+++ shop/views.py
@@ -6,9 +6,8 @@ from .models import Product, Discount, Category, Comment, Customer, Address, Car
 
 
 def some_view(request):
-    queryset = Product.objects.filter(category__id=620)
-    queryset = Product.objects.filter(category__title__icontains='think')
-    queryset = Address.objects.filter(customer__first_name__icontains='jay')
-
+    queryset = Product.objects.filter(datetime_created__year=2022)
+    queryset = Product.objects.filter(datetime_created__month=10)
+    queryset = Product.objects.filter(datetime_created__day=1)
     
     return render(request, 'shop/shop.html', {'products': list(queryset)})
