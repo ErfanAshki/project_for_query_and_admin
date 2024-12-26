3424a41c3bdbd3dba3de7273ad278266b492bb37
erfan ashki <erfanbayern111@gmail.com>
2024-12-26 19:58:21 +0330

    annotate with CONCAT Func

--- shop/views.py
+++ shop/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from decimal import Decimal
-from django.db.models import Q, F, Avg, Count, Max, Min, Sum, Value
+from django.db.models import Q, F, Avg, Count, Max, Min, Sum, Value, Func, ExpressionWrapper, DecimalField
 
 from .models import Product, Discount, Category, Comment, Customer, Address, Cart, CartItem, Order, OrderItem
 
 
 def some_view(request):
-    queryset = Category.objects.annotate(new_column=Value(55)).all()
-    queryset = Category.objects.annotate(new_column=Value('sss')).all()
-    queryset = Product.objects.annotate(price=F('unit_price')).all()
-    queryset = OrderItem.objects.annotate(total_price=F('unit_price') * F('quantity')).all()
+    queryset = Customer.objects.annotate(full_name=Func(F('first_name'), Value(' '), F('last_name'), \
+                                        function='CONCAT')).defer('first_name', 'last_name').all()
     
     return render(request, 'shop/shop.html', {'orders': list(queryset)})
