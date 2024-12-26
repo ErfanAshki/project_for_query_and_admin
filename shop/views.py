25252d74800182ffa2b2ad86dcf7eed5f12092a5
erfan ashki <erfanbayern111@gmail.com>
2024-12-26 16:33:04 +0330

    Count & Avg aggregations

--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from decimal import Decimal
-from django.db.models import Q, F
+from django.db.models import Q, F, Avg, Count
 
 from .models import Product, Discount, Category, Comment, Customer, Address, Cart, CartItem, Order, OrderItem
 
 
 def some_view(request):
-    queryset = Order.objects.prefetch_related('items__product').select_related('customer').all()
+    queryset = Product.objects.aggregate(Count('inventory'))
+    queryset = Comment.objects.aggregate(Count('status'))
+    queryset = OrderItem.objects.aggregate(Count('id'))
+    queryset = OrderItem.objects.aggregate(Avg('quantity'))
+    queryset = Product.objects.aggregate(Avg('inventory'))
+    queryset = Product.objects.aggregate(Avg('unit_price'))
+    queryset = Product.objects.aggregate(number_of_products=Count('inventory'))
+    queryset = Product.objects.aggregate(average_of_price=Avg('unit_price'))
     
     return render(request, 'shop/shop.html', {'orders': list(queryset)})
