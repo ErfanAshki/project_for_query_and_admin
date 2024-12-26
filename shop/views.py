63182c6bfcad4c192eba0419dc5c151e635943dd
erfan ashki <erfanbayern111@gmail.com>
2024-12-26 16:57:48 +0330

    Max & Min & Sum aggregations

--- shop/views.py
+++ shop/views.py
@@ -1,19 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from decimal import Decimal
-from django.db.models import Q, F, Avg, Count
+from django.db.models import Q, F, Avg, Count, Max, Min, Sum
 
 from .models import Product, Discount, Category, Comment, Customer, Address, Cart, CartItem, Order, OrderItem
 
 
 def some_view(request):
-    queryset = Product.objects.aggregate(Count('inventory'))
-    queryset = Comment.objects.aggregate(Count('status'))
-    queryset = OrderItem.objects.aggregate(Count('id'))
-    queryset = OrderItem.objects.aggregate(Avg('quantity'))
-    queryset = Product.objects.aggregate(Avg('inventory'))
-    queryset = Product.objects.aggregate(Avg('unit_price'))
-    queryset = Product.objects.aggregate(number_of_products=Count('inventory'))
-    queryset = Product.objects.aggregate(average_of_price=Avg('unit_price'))
+    queryset = Product.objects.aggregate(max_price=Max('unit_price'))
+    queryset = Product.objects.aggregate(min_inventory=Min('inventory'))
+    queryset = Product.objects.aggregate(sum_inventory=Sum('inventory'))
     
     return render(request, 'shop/shop.html', {'orders': list(queryset)})
