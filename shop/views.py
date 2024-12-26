5a04aff72ec903a011b70cc4d1698b5d6f9d9f60
erfan ashki <erfanbayern111@gmail.com>
2024-12-26 19:28:16 +0330

    annotate with Value and F objects

--- shop/views.py
+++ shop/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from decimal import Decimal
-from django.db.models import Q, F, Avg, Count, Max, Min, Sum
+from django.db.models import Q, F, Avg, Count, Max, Min, Sum, Value
 
 from .models import Product, Discount, Category, Comment, Customer, Address, Cart, CartItem, Order, OrderItem
 
 
 def some_view(request):
-    queryset = Product.objects.filter(inventory__gt=10).aggregate(number=Count('id'), avg_price=Avg('unit_price'))
-    queryset = OrderItem.objects.filter(product_id=4599).aggregate(number=Count('id'))
+    queryset = Category.objects.annotate(new_column=Value(55)).all()
+    queryset = Category.objects.annotate(new_column=Value('sss')).all()
+    queryset = Product.objects.annotate(price=F('unit_price')).all()
+    queryset = OrderItem.objects.annotate(total_price=F('unit_price') * F('quantity')).all()
     
-    order_item_queryset = OrderItem.objects.values('product_id').distinct()
-    queryset = Product.objects.filter(id__in=order_item_queryset).aggregate(number_of_product=Count('id'))
-
     return render(request, 'shop/shop.html', {'orders': list(queryset)})
