429e32c32408cc2f4718be2159f0a2d56826cad7
erfan ashki <erfanbayern111@gmail.com>
2024-12-28 21:57:22 +0330

    implement OrderAdmin

--- shop/admin.py
+++ shop/admin.py
@@ -4,10 +4,20 @@ from .models import Product, Cart, CartItem, Category, Comment, Customer ,Order,
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'category', 'unit_price', 'inventory', 'datetime_created']
+    list_display = ['id', 'name', 'category', 'unit_price', 'inventory', 'datetime_created']
     ordering = ['id']
     list_per_page = 20    
     list_editable = ['unit_price', 'inventory']
     
     
 admin.site.register(Product, ProductAdmin)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'customer', 'status', 'datetime_created']
+    ordering = ['id']
+    list_per_page = 15    
+    list_editable = ['status']
+    
+    
+admin.site.register(Order, OrderAdmin)
