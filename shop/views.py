499f1863dcb33909722722b8e950379ddd18672e
erfan ashki <erfanbayern111@gmail.com>
2024-12-28 20:56:11 +0330

    use prefetch class in prefetch_related

--- shop/views.py
+++ shop/views.py
@@ -1,22 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from decimal import Decimal
-from django.db.models import Q, F, Avg, Count, Max, Min, Sum, Value, Func, ExpressionWrapper, DecimalField
+from django.db.models import Q, F, Avg, Count, Max, Min, Sum, Value, Func,ExpressionWrapper,DecimalField,Prefetch
 from django.db import transaction
 
 from .models import Product, Discount, Category, Comment, Customer, Address, Cart, CartItem, Order, OrderItem
 
 
-# @transaction.atomic()
 def some_view(request):
-    with transaction.atomic():
-        order = Order.objects.create(customer_id=311)
-
-        order_item1 = OrderItem.objects.create(
-            order=order,
-            product_id=4500.
-            quantity=5,
-            unit_price=100
-        )
+    queryset = Order.objects.prefetch_related(Prefetch( \
+        'items', queryset=OrderItem.objects.select_related('product'))) \
+        .annotate(order_items=Count('items'))
     
-    return render(request, 'shop/shop.html', {'products': list(queryset)})
+    return render(request, 'shop/shop.html', {'orders': list(queryset)})
