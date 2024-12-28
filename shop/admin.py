a6b21707053933be0ff74d4866b3d4db8198b5a5
erfan ashki <erfanbayern111@gmail.com>
2024-12-28 23:53:18 +0330

    add category_title computed filed to ProductAdmin with apply ordering operation

--- shop/admin.py
+++ shop/admin.py
@@ -4,10 +4,11 @@ from .models import Product, Cart, CartItem, Category, Comment, Customer ,Order,
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'category', 'unit_price', 'inventory', 'inventory_status', 'datetime_created']
+    list_display = ['id', 'name', 'category_title' , 'unit_price', 'inventory', 'inventory_status', 'datetime_created']
     ordering = ['id']
     list_per_page = 20    
     list_editable = ['unit_price', 'inventory']
+    list_select_related = ['category']
     
     def inventory_status(self, product):
         if product.inventory < 10 :
@@ -16,6 +17,10 @@ class ProductAdmin(admin.ModelAdmin):
             return 'HIGH'
         else:
             return 'MEDIUM'
+        
+    @admin.display(ordering='category__title')
+    def category_title(self , product):
+        return product.category.title
         
     
 admin.site.register(Product, ProductAdmin)
--- shop/models.py
+++ shop/models.py
@@ -9,6 +9,9 @@ class Category(models.Model):
     description = models.TextField(verbose_name=_('body'), blank=True)
     top_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
     
+    def __str__(self):
+        return self.title
+    
 
 class Discount(models.Model):
     discount = models.FloatField(verbose_name=_('discount'))
