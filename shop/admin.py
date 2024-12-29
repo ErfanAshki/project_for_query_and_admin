5dfe06429c3c816c9f4447b85777136390628d32
erfan ashki <erfanbayern111@gmail.com>
2024-12-29 14:22:07 +0330

    add custom InventoryFilter to ProductAdmin

--- shop/admin.py
+++ shop/admin.py
@@ -4,13 +4,36 @@ from django.db.models import Count
 from .models import Product, Cart, CartItem, Category, Comment, Customer ,Order, OrderItem, Discount, Address
 
 
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'Critical Inventory Status'
+    parameter_name = 'inventory'
+    LESS_THAN_3 = '<3'
+    BETWEEN_3_AND_10 = '3<=10'
+    GREATER_THAN_10 = '>10'
+
+    def lookups(self, request, model_admin):
+        return [
+            (InventoryFilter.LESS_THAN_3, 'High'),
+            (InventoryFilter.BETWEEN_3_AND_10, 'Medium'),
+            (InventoryFilter.GREATER_THAN_10, 'Ok')
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == InventoryFilter.LESS_THAN_3:
+            return queryset.filter(inventory__lt=3)
+        elif self.value() == InventoryFilter.BETWEEN_3_AND_10:
+            return queryset.filter(inventory__range=(3, 10))
+        elif self.value() == InventoryFilter.GREATER_THAN_10:
+            return queryset.filter(inventory__gt=10)
+
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'category_title' , 'unit_price', 'inventory', 'inventory_status', 'datetime_created']
     ordering = ['id']
     list_per_page = 20    
     list_editable = ['unit_price', 'inventory']
     list_select_related = ['category']
-    list_filter = ['category', 'inventory']
+    list_filter = [InventoryFilter, 'category']
     
     def inventory_status(self, product):
         if product.inventory < 10 :
